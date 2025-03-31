3d6c145b6fda9a936eaa28b34136c8579f6b8a28
Sonal Singh <sonal1201kumar@gmail.com>
2025-03-31 11:26:53 +0530

    Added Routes structure

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import express from "express"
 import jwt from "jsonwebtoken"
+import apiRouter from "./routes"
 
 const app = express()
 
 
+app.use('/api',apiRouter)
+
+app.listen(3000,()=>{
+    console.log(":ksdjviudf")
+})
---
+++ src/routes/v1/index.ts
@@ -0,0 +1,10 @@
+import express from "express"
+
+const v1Router = express.Router();
+
+v1Router.use('/auth', )
+v1Router.use('/tag', )
+v1Router.use('/content', )
+v1Router.use('/link', )
+
+export default v1Router;
